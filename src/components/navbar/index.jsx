6cd906f9049d1dc37da437be283df2b481c61e38
leestanley <stanleylee91776@gmail.com>
2020-11-17 11:33:19 -0800

    removed links

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -7,13 +7,6 @@ function Navbar() {
       <div className="nav_wrapper">
         <ul>
           <a
-            href="https://drive.google.com/file/d/1cEU7myTYPqcKQYlQvNRfn6wLqVFRTdLb/view"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <li>resume</li>
-          </a>
-          <a
             href="https://devpost.com/leestanley"
             target="_blank"
             rel="noopener noreferrer"
